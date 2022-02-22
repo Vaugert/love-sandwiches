8b6f55c68a4c8910b718e4e0f93d0b81e03f5010
Vaugert <lindstromfabian@gmail.com>
2022-02-22 11:43:03 +0000

    add requirements for deployment

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -26,7 +25,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here:\n ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
 
@@ -101,10 +100,12 @@ def get_last_5_entries_sales():
 
     return columns
 
+
 def calculate_stock_data(data):
-    """calculate athea varage stock for each item type, adding 10%
     """
-    print("calculating stokc data...\n")
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
     new_stock_data = []
 
     for column in data:
@@ -132,5 +133,3 @@ def main():
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
-
